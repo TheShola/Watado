6952ad493d9964299af67f85793eabc56bf2533c
theshola <adesolaabimbola@gmail.com>
2017-08-05 00:56:24 -0400

    Set Up of React, Babel and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    path: '/Users/Shola/Documents/WATADO/public',
+    filename: 'bundle.js'       
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {  
+    extensions: [' ' , '.js' , '.json']
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+};
